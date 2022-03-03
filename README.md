ceda63e2b823122642507e8f2ddb03026fb60fd1
Mircea Dinoiu <mircea@dinoiu.com>
2022-03-02 16:32:20 -0800

    first

---
+++ README.md
@@ -0,0 +1,9 @@
+# OFX/QFX to CSV
+
+Some accounts can export OFX/QFX but do not have a Quicken license, therefore cannot be imported.  
+By converting to Mint's CSV format, there is no license check stopping one from importing.
+
+```
+$ node . --file ~/Downloads/file.qfx --account-name 'My Checking Account'
+```
+The above command will generate a `~/Downloads/file.csv` file that can be imported.
---
+++ index.js
@@ -0,0 +1,73 @@
+const {argv} = require('yargs');
+const ofx = require('ofx');
+const path = require('path');
+const fs = require('fs');
+const isPlainObject = require('lodash.isplainobject');
+const {stringify} = require('csv-stringify/sync');
+const moment = require('moment');
+
+const main = () => {
+    const filePath = argv.file;
+    const accountName = argv.accountName;
+
+    const fileContents = fs.readFileSync(filePath, 'utf-8')
+    const transactions = mapOfxToTransactions(ofx.parse(fileContents.toString()));
+    const output = stringify([
+        [
+            'Date',
+            'Description',
+            'Original Description',
+            'Amount',
+            'Transaction Type',
+            'Category',
+            'Account Name',
+            'Labels',
+            'Notes',
+        ],
+        ...transactions.map((attrs) => {
+            const {TRNTYPE, DTPOSTED, TRNAMT, FITID, NAME} = attrs;
+            const sum = Number(TRNAMT);
+
+            return [
+                moment(DTPOSTED, 'YYYYMMDDHHmmss')
+                    .parseZone().format('M/D/YYYY'),
+                NAME,
+                NAME,
+                Math.abs(sum),
+                sum < 0 ? 'debit' : 'credit',
+                '',
+                accountName,
+                '',
+                '',
+            ];
+        }),
+    ]);
+
+    const fileParsed = path.parse(filePath);
+    const outputFile = path.join(fileParsed.root, fileParsed.dir, fileParsed.name + '.csv');
+
+    fs.writeFileSync(outputFile, output);
+
+    console.log(output);
+}
+
+const mapOfxToTransactions = (data) => {
+    if (!data) {
+        return [];
+    }
+
+    if (data.BANKTRANLIST) {
+        return data.BANKTRANLIST.STMTTRN;
+    }
+
+    if (isPlainObject(data)) {
+        return Object.values(data).reduce((acc, each) => {
+            return acc.concat(mapOfxToTransactions(each));
+        }, []);
+    }
+
+    return [];
+};
+
+
+main();
